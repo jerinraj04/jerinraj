f942b5e1f31a64ac71fb9ba84cbe50f6e5711b0f
jerinraj04 <42570606+jerinraj04@users.noreply.github.com>
2019-01-08 14:39:16 +0530

    Update jerin2.py

--- jerin2.py
+++ jerin2.py
@@ -1,29 +1,12 @@
-# Function to find the smallest number 
+
 def smallest(lst): 
-      
-    # Here i is index and n is the number of the list 
-    for i,n in enumerate(lst):  
-          
-        # Checking for the first non-zero digit in the sorted list 
-        if n != '0':  
-              
-            # Remove and store the digit from the lst 
-            tmp = lst.pop(i) 
-            break
-      
-    # Place the first non-zero digit at the starting 
-    # and return the final number 
-    return str(tmp) + ''.join(lst)  
-  
-  
-# Driver program 
-if __name__ == '__main__': 
-      
-    # Converting the given numbers into string to form a list 
+      for i,n in enumerate(lst):  
+    if n != '0':  
+        tmp = lst.pop(i) 
+        break
+   return str(tmp) + ''.join(lst)  
+ if name == 'main': 
     lst = list(str(570107)) 
     lst.sort() 
+  print smallest(lst) 
       
-    # Calling the function using the above list 
-    print smallest(lst) 
-      
-# This code is contributed by Mahendra Yadav 
