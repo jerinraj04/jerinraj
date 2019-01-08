80d6a2d21cac8a28a9e14f146d467c99d06bd25d
jerinraj04 <42570606+jerinraj04@users.noreply.github.com>
2019-01-08 14:42:30 +0530

    Update jerin3.py

--- jerin3.py
+++ jerin3.py
@@ -1,30 +1,13 @@
-# Python3 Program to find  
-# whether a no is  
-# power of two 
 import math 
-  
-# Function to check 
-# Log base 2 
 def Log2(x): 
-    return (math.log10(x) / 
-            math.log10(2)); 
-  
-# Function to check 
-# if x is power of 2 
+    return (math.log10(x)/ math.log10(2)); 
 def isPowerOfTwo(n): 
-    return (math.ceil(Log2(n)) == 
-            math.floor(Log2(n))); 
-  
-# Driver Code 
-if(isPowerOfTwo(31)): 
+    return (math.ceil(Log2(n)) == math.floor(Log2(n))); 
+ if(isPowerOfTwo(31)): 
     print("Yes"); 
 else: 
     print("No"); 
-  
-if(isPowerOfTwo(64)): 
+ if(isPowerOfTwo(64)): 
     print("Yes"); 
 else: 
     print("No"); 
-      
-# This code is contributed  
-# by mits 
