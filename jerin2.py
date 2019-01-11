e2c8403c18fc65023c93e1b78b1f8667f0b31b62
jerinraj04 <42570606+jerinraj04@users.noreply.github.com>
2019-01-11 10:31:37 +0530

    Add files via upload

--- jerin2.py
+++ jerin2.py
@@ -1,12 +1,21 @@
-
-def smallest(lst): 
-      for i,n in enumerate(lst):  
-    if n != '0':  
-        tmp = lst.pop(i) 
-        break
-   return str(tmp) + ''.join(lst)  
- if name == 'main': 
-    lst = list(str(570107)) 
-    lst.sort() 
-  print smallest(lst) 
+# Python3 code to round the given  
+# integer to a whole number  
+# which ends with zero. 
+  
+# function to round the number 
+def round( n ): 
+  
+    # Smaller multiple 
+    a = (n // 10) * 10
       
+    # Larger multiple 
+    b = a + 10
+      
+    # Return of closest of two 
+    return (b if n - a > b - n else a) 
+  
+# driver code 
+n = 4722
+print(round(n)) 
+  
+# This code is contributed by "Sharad_Bhardwaj".
