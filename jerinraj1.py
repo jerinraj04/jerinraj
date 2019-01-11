e92acb5750b47ddfd01eb71ed9e6ee937bbc3c03
jerinraj04 <42570606+jerinraj04@users.noreply.github.com>
2019-01-11 11:58:10 +0530

    Update jerinraj1.py

--- jerinraj1.py
+++ jerinraj1.py
@@ -1,22 +1,17 @@
-#include <stdio.h>
 int main()
 {
-    int n, i, flag = 0;
-
-    printf("Enter a positive integer: ");
-    scanf("%d", &n);
-
-    for(i = 2; i <= n/2; ++i)
+ int n, i, flag = 0;
+printf("Enter a positive integer: ");
+scanf("%d", &n);
+for(i = 2; i <= n/2; ++i)
     {
-        // condition for nonprime number
-        if(n%i == 0)
+       if(n%i == 0)
         {
             flag = 1;
             break;
         }
     }
-
-    if (n == 1) 
+ if (n == 1) 
     {
       printf("1 is neither a prime nor a composite number.");
     }
@@ -27,7 +22,6 @@ int main()
         else
           printf("%d is not a prime number.", n);
     }
-    
     return 0;
 }
 
