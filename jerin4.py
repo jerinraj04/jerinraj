755a421c627681283f83da5dd5c6b0023c64586f
jerinraj04 <42570606+jerinraj04@users.noreply.github.com>
2019-01-11 10:40:53 +0530

    Update jerin4.py

--- jerin4.py
+++ jerin4.py
@@ -1,10 +1,5 @@
-
-# Python implementation of the above approach 
 N = 6
-  
- # number of even numbers 
 Even = N//2
-  
-# number of odd numbers 
 Odd = N-Even  
 print(Even * Odd) 
+# number of odd numbers 
