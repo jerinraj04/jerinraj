9f07f7a9cec06af81823e1979ff7e079313d711f
jerinraj04 <42570606+jerinraj04@users.noreply.github.com>
2019-01-11 12:13:22 +0530

    Update jerinraj5.py

--- jerinraj5.py
+++ jerinraj5.py
@@ -1,37 +1,15 @@
-# Python3 implementation to check if 
-# characters of a given string can 
-# be rearranged to form a palindrome 
-  
-NO_OF_CHARS = 256
-  
-# function to check whether characters 
-# of a string can form a palindrome  
-def canFormPalindrome(st) : 
-  
-    # Create a count array and initialize   
-    # all values as 0 
+NO OF CHARS = 256
+  def canFormPalindrome(st) : 
     count = [0] * (NO_OF_CHARS) 
-  
-    # For each character in input strings, 
-    # increment count in the corresponding 
-    # count array 
-    for i in range( 0, len(st)) : 
+ for i in range( 0, len(st)) : 
         count[ord(st[i])] = count[ord(st[i])] + 1
-  
-    # Count odd occurring characters 
-    odd = 0
-      
-    for i in range(0, NO_OF_CHARS ) : 
+ odd = 0
+ for i in range(0, NO_OF_CHARS ) : 
         if (count[i] & 1) : 
             odd = odd + 1
-  
-        if (odd > 1) : 
+  if (odd > 1) : 
             return False
-              
-    # Return true if odd count is 0 or 1,  
-    return True
-  
-# Driver program 
+              return True
 if(canFormPalindrome("geeksforgeeks")) : 
     print("Yes") 
 else : 
@@ -42,4 +20,4 @@ if(canFormPalindrome("geeksogeeks")) :
 else : 
     print("No") 
   
-# This code is contributed by Nikita Tiwari. 
+
